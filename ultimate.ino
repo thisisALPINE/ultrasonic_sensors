a98aa4e47aed40f928f1d9b3249e88ad9f82ab21
Michelangelo <mike.prisciano@icloud.com>
2017-12-19 22:23:37 +0100

    Add files via upload
    added melody at start

--- ultimate.ino
+++ ultimate.ino
@@ -1,3 +1,15 @@
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
 //HC RS04 Sensore ultrasuoni
 int triggerPort = 7;
 int echoPort = 8;
@@ -27,6 +39,22 @@ pinMode(buzzer_two, OUTPUT);
 
 Serial.begin( 9600 );
 Serial.println( "Sensore ultrasuoni: ");
+
+// iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
  
 }
  
